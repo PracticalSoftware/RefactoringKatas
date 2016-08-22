b8c23abe822e3eb1f690c44545cbf9ff578e9cdc
itzik saban <itziksaban@gmail.com>
2016-08-22 22:13:17 +0300

    Step 2

--- JordenRiver/JordenRiver/CartService.cs
+++ JordenRiver/JordenRiver/CartService.cs
@@ -12,20 +12,17 @@ namespace JordanRiver
 
             //Fetch the cart details from a json file.
             var path = @"c:\cart" + cartId + ".json";
-            using (FileStream fs = File.Open(path, FileMode.Open))
-            {
-                using (StreamReader sr = new StreamReader(fs))
-                using (JsonReader jr = new JsonTextReader(sr))
-                {
-                    JsonSerializer serializer = new JsonSerializer();
-                    cart = serializer.Deserialize<Cart>(jr);
-                }
-            }
+            cart = Open(path);
 
             //Add the item to the cart
             cart.Items.Add(item);
 
             //Store again the update cart in the json file.
+            Save(path, cart);
+        }
+
+        public void Save(string path, Cart cart)
+        {
             using (FileStream fs = File.Open(path, FileMode.Open))
             {
                 using (StreamWriter sw = new StreamWriter(fs))
@@ -37,6 +34,21 @@ namespace JordanRiver
                     serializer.Serialize(jw, cart);
                 }
             }
+        }
+
+        public Cart Open(string path)
+        {
+            Cart cart;
+            using (FileStream fs = File.Open(path, FileMode.Open))
+            {
+                using (StreamReader sr = new StreamReader(fs))
+                using (JsonReader jr = new JsonTextReader(sr))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    cart = serializer.Deserialize<Cart>(jr);
+                }
+            }
+            return cart;
         }
     }
 }
